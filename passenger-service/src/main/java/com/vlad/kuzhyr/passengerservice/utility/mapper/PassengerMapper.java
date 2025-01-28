2774015b956fb4ef799a002dbc59ea9fa41e2664
XD-cods <vkuzir7@gmail.com>
2025-01-28 13:14:47 +0300

    update passengerMapper imports

--- passenger-service/src/main/java/com/vlad/kuzhyr/passengerservice/utility/mapper/PassengerMapper.java
+++ passenger-service/src/main/java/com/vlad/kuzhyr/passengerservice/utility/mapper/PassengerMapper.java
@@ -3,7 +3,11 @@ package com.vlad.kuzhyr.passengerservice.utility.mapper;
 import com.vlad.kuzhyr.passengerservice.persistence.entity.Passenger;
 import com.vlad.kuzhyr.passengerservice.web.request.PassengerRequest;
 import com.vlad.kuzhyr.passengerservice.web.response.PassengerResponse;
-import org.mapstruct.*;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(
         componentModel = "spring",
