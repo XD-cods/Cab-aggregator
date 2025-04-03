0e5f0df21334ba3c1a724a3c408224549b7bc964
XD-cods <vkuzir7@gmail.com>
2025-04-03 10:50:20 +0300

    Update metrics

--- ride-service/src/main/java/com/vlad/kuzhyr/rideservice/utility/constant/ExceptionMessageConstant.java
+++ ride-service/src/main/java/com/vlad/kuzhyr/rideservice/utility/constant/ExceptionMessageConstant.java
@@ -35,5 +35,6 @@ public final class ExceptionMessageConstant {
 
     public static final String DRIVER_SERVICE_IS_CURRENTLY_UNAVAILABLE = "Driver service is currently unavailable";
 
-    public static final String PASSENGER_SERVICE_IS_CURRENTLY_UNAVAILABLE = "Passenger service is currently unavailable";
+    public static final String PASSENGER_SERVICE_IS_CURRENTLY_UNAVAILABLE = "Passenger service is currently" +
+                                                                            " unavailable";
 }
